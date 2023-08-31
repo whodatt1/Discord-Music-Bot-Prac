547c59ae8401c3b028b52edeb6c967e06eda0a5a
whodatt1 <99710312+whodatt1@users.noreply.github.com>
2023-08-31 22:50:06 +0900

    #. AudioHandler, GuildMusicManager 수정

--- IGBOT/src/main/java/com/example/demo/audio/AudioHandler.java
+++ IGBOT/src/main/java/com/example/demo/audio/AudioHandler.java
@@ -2,9 +2,19 @@ package com.example.demo.audio;
 
 import java.nio.ByteBuffer;
 
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.track.playback.AudioFrame;
+
 import net.dv8tion.jda.api.audio.AudioSendHandler;
 
 public class AudioHandler implements AudioSendHandler {
+	
+	private AudioPlayer audioPlayer;
+	private AudioFrame lastFrame;
+	
+	public AudioHandler(AudioPlayer audioPlayer) {
+		this.audioPlayer = audioPlayer;
+	}
 	
 	@Override
 	public boolean canProvide() {
--- IGBOT/src/main/java/com/example/demo/audio/GuildMusicManager.java
+++ IGBOT/src/main/java/com/example/demo/audio/GuildMusicManager.java
@@ -1,5 +1,24 @@
 package com.example.demo.audio;
 
-public class GuildMusicManager {
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 
+public class GuildMusicManager {
+	
+	// 길드 오디오 플레이어
+	public final AudioPlayer player;
+	
+	// 플레이어 스케쥴러
+	public final TrackScheduler scheduler;
+	
+	// 길드 오디오 플레이어, 플레이어 스케쥴러 생성
+	public GuildMusicManager(AudioPlayerManager manager) {
+		player = manager.createPlayer();
+		scheduler = new TrackScheduler(player);
+		player.addListener(scheduler);
+	}
+	
+	public AudioHandler getAudioHandler() {
+		return new AudioHandler(player);
+	}
 }
