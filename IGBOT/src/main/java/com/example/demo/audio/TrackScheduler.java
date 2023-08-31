12e058fe70d66abaa2420d96990cd67253517a60
whodatt1 <99710312+whodatt1@users.noreply.github.com>
2023-08-31 22:41:09 +0900

    #. Track 관리를 위한 TrackScheduler 클래스 추가

---
+++ IGBOT/src/main/java/com/example/demo/audio/TrackScheduler.java
@@ -0,0 +1,58 @@
+package com.example.demo.audio;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TrackScheduler extends AudioEventAdapter {
+	private boolean repeating = false;
+	private final AudioPlayer player;
+	private final BlockingQueue<AudioTrack> queue;
+	private AudioTrack lastTrack;
+	
+	public TrackScheduler(AudioPlayer player) {
+		this.player = player;
+		this.queue = new LinkedBlockingQueue<>();
+	}
+	
+	// 노래 스킵
+	public void skip() {
+		player.startTrack(queue.poll(), false);
+	}
+	
+	@Override
+	public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason) {
+		this.lastTrack = track;
+		
+		if (endReason.mayStartNext) {
+			// 반복 허용시
+			if (repeating) {
+				player.startTrack(lastTrack.makeClone(), false);
+			} else {
+				skip();
+			}
+		}
+	}
+	
+	// 노래 섞기
+	public void shuffle() {
+		List<AudioTrack> list = new ArrayList<>(queue);
+		queue.clear();
+		Collections.shuffle(list);
+		for (AudioTrack audioTrack : list) {
+			queue.add(audioTrack);
+		}
+	}
+}
